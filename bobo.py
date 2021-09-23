630d1672f3a44540da26486de005a48ab2d19414
Jim Anderson <jima.coding@gmail.com>
2021-09-22 20:45:49 -0600

    Moved data to json file

--- bobo.py
+++ bobo.py
@@ -1,49 +1,18 @@
 #!/usr/bin/env python3
+import json
 import PySimpleGUI as sg
 sg.theme('Dark Amber')    # Add some color for fun
 
-info = [
-   {"date" : "03/09/2021", "series" : "",                "title" : "The Mirror and The Light",   "author" : "Hilary Mantel" },
-   {"date" : "03/13/2021", "series" : "Memory Man",      "title" : "The Fix",                    "author" : "David Baldacci" },
-   {"date" : "03/15/2021", "series" : "Jack Reacher",    "title" : "Worth Dying For",            "author" : "Lee Child" },
-   {"date" : "03/19/2021", "series" : "Memory Man",      "title" : "The Last Mile",              "author" : "David Baldacci" },
-   {"date" : "03/25/2021", "series" : "Memory Man",      "title" : "Redemption",                 "author" : "David Baldacci" },
-   {"date" : "03/29/2021", "series" : "",                "title" : "Dance Hall of the Dead",     "author" : "Tony Hillerman" },
-   {"date" : "04/04/2021", "series" : "Jack Reacher",    "title" : "Night School",               "author" : "Lee Child" },
-   {"date" : "04/09/2021", "series" : "Atlee Pine",      "title" : "A Minute To Midnight",       "author" : "David Baldacci" },
-   {"date" : "04/17/2021", "series" : "Archer",          "title" : "One Good Deed",              "author" : "David Baldacci" },
-   {"date" : "04/28/2021", "series" : "",                "title" : "Chances Are",                "author" : "Richard Russo" },
-   {"date" : "05/05/2021", "series" : "Jack Reacher",    "title" : "A Wanted Man",               "author" : "Lee Child" },
-   {"date" : "05/12/2021", "series" : "",                "title" : "All the Devils Are Here",    "author" : "Louise Penny" },
-   {"date" : "05/18/2021", "series" : "Will Robie",      "title" : "The Innocent",               "author" : "David Baldacci" },
-   {"date" : "05/xx/2021", "series" : "Will Robie",      "title" : "The Target",                 "author" : "David Baldacci" },
-   {"date" : "06/09/2021", "series" : "Jack Reacher",    "title" : "The Hard Way",               "author" : "Lee Child" },
-   {"date" : "06/16/2021", "series" : "Jesse Stone",     "title" : "Night Passage",              "author" : "Robert B. Parker" },
-   {"date" : "06/20/2021", "series" : "Spencer",         "title" : "God Save the Child",         "author" : "Robert B. Parker" },
-   {"date" : "06/23/2021", "series" : "Jesse Stone",     "title" : "Night and Day",              "author" : "Robert B. Parker" },
-   {"date" : "07/01/2021", "series" : "Sunny Randall",   "title" : "Family Honor",               "author" : "Robert B. Parker" },
-   {"date" : "07/02/2021", "series" : "Sunny Randall",   "title" : "Perish Twice",               "author" : "Robert B. Parker" },
-   {"date" : "07/03/2021", "series" : "Sunny Randall",   "title" : "Shrink Rap",                 "author" : "Robert B. Parker" },
-   {"date" : "07/04/2021", "series" : "Sunny Randall",   "title" : "Melancholy Baby",            "author" : "Robert B. Parker" },
-   {"date" : "07/05/2021", "series" : "Sunny Randall",   "title" : "Blue Screen",                "author" : "Robert B. Parker" },
-   {"date" : "07/08/2021", "series" : "Sunny Randall",   "title" : "Spare Change",               "author" : "Robert B. Parker" },
-   {"date" : "07/10/2021", "series" : "Jesse Stone",     "title" : "Killing the Blues",          "author" : "Michael Brandman" },
-   {"date" : "07/11/2021", "series" : "Jesse Stone",     "title" : "Damned if you Do",           "author" : "Michael Brandman" },
-   {"date" : "07/16/2021", "series" : "Jesse Stone",     "title" : "The Devil Wins",             "author" : "Reed Farrel Coleman" },
-   {"date" : "07/18/2021", "series" : "Jesse Stone",     "title" : "Split Image",                "author" : "Robert B. Parker" },
-   {"date" : "07/20/2021", "series" : "Spenser",         "title" : "Looking For Rachel Wallace", "author" : "Robert B. Parker" },
-   {"date" : "07/28/2021", "series" : "Jack Reacher",    "title" : "The Sentinel",               "author" : "Lee Child" },
-   {"date" : "07/30/2021", "series" : "Spenser",         "title" : "The Godwulf Manuscript",     "author" : "Robert B. Parker" },
-   {"date" : "08/01/2021", "series" : "Jesse Stone",     "title" : "The Bitterest Pill",         "author" : "Reed Farrel Coleman" },
-   {"date" : "08/07/2021", "series" : "Jesse Stone",     "title" : "The Hangman's Sonnet",       "author" : "Reed Farrel Coleman" },
-   {"date" : "08/15/2021", "series" : "Jack Reacher",    "title" : "One Shot",                   "author" : "Lee Child" },
-   {"date" : "08/19/2021", "series" : "Jesse Stone",     "title" : "High Profile",               "author" : "Robert B. Parker" },
-   {"date" : "08/20/2021", "series" : "Jesse Stone",     "title" : "Stranger in Paradise",       "author" : "Robert B. Parker" },
-   {"date" : "08/22/2021", "series" : "Spenser",         "title" : "Early Autumn",               "author" : "Robert B. Parker" },
-   {"date" : "08/31/2021", "series" : "Will Robie",      "title" : "The Guilty",                 "author" : "David Baldacci" },
-   {"date" : "09/03/2021", "series" : "Matthew Scudder", "title" : "The Sins of the Father",     "author" : "Lawrence Block" },
-   {"date" : "09/07/2021", "series" : "Matthew Scudder", "title" : "In the Midst of Death",      "author" : "Lawrence Block" },
-]
+database_file = "book.json"
+def read_data():
+    with open(database_file, "r") as fp:
+        return json.load(fp)
+
+def write_data(info):
+    with open(database_file, "w") as fp:
+        json.dump(info, fp, indent=2)
+
+info = read_data()
 data = [[item["date"], item["author"], item["title"], item["series"]] for item in sorted(info, key=lambda x: x["date"], reverse=True)]
 
 # Todo - make author and series filter drop-downs
