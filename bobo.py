8bcf96d521fa55d65495deb714fd17cac80b3d97
Jim Anderson <jima.coding@gmail.com>
2021-09-23 20:26:52 -0600

    got author filter working

--- bobo.py
+++ bobo.py
@@ -20,9 +20,12 @@ data = [[item["date"], item["author"], item["title"], item["series"]] for item i
 # get keyboard shortcuts working
 
 
+authors = {item["author"] for item in sorted(info, key=lambda x: x["author"])}
+
 # 1- the layout
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Input(key='-AUTHOR-'), sg.Input(key="-SERIES-")],
+# layout = [[sg.Text("Filter by author:"), sg.Listbox(values=authors, size=(20,4), enable_events=True)],
+# layout = [[sg.Listbox(values=list(authors), size=(20,4), enable_events=True)],
+layout = [[sg.Text("Filter by author:"), sg.Listbox(values=list(authors), size=(20,4), key='-AUTHORS-', enable_events=True)],
           [sg.Table(
                 values=data,
                 headings=["Date Read", "Author", "Title", "Series", ],
@@ -39,20 +42,20 @@ window = sg.Window('Pattern 2', layout)
 # 3 - the event loop
 while True:
     event, values = window.read()
-    print(event, values)
+    print(f"{event}", values)
+    print(values["-AUTHORS-"][0])
+    # print(window["-AUTHORS-"])
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == 'Show':
+    if event == 'Show' or event == '-AUTHORS-':
         # Update the "output" text element to be the value of "input" element
-        print("author", values["-AUTHOR-"])
-        print("series", values["-SERIES-"])
-        author = values["-AUTHOR-"]
-        series = values["-SERIES-"]
+        author = values["-AUTHORS-"][0]
+        # series = values["-SERIES-"]
         data = [[item["date"], item["author"], item["title"], item["series"]] for item in sorted(info, key=lambda x: x["date"], reverse=True)]
         if author:
             data = [x for x in data if x[1] == author]
-        if series:
-            data = [x for x in data if x[3] == series]
+        # if series:
+            # data = [x for x in data if x[3] == series]
 
         # window['-OUTPUT-'].update(values['-AUTHOR-'])
         window["-BOOKTABLE-"].update(data)
