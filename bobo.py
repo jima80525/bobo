22f464ec070adf8c062b5459208fc1e29c43cd9c
Jim Anderson <jima80525@gmail.com>
2021-12-04 08:50:41 -0700

    Got filtering by author and/or series working

--- bobo.py
+++ bobo.py
@@ -7,7 +7,7 @@ import shutil
 import sys
 import time
 
-sg.theme("Dark Amber")  # Add some color for fun
+sg.theme("Dark Amber")
 
 # TODO
 # * add way to search for author or series in upper boxes - big lists is unwieldy
@@ -41,17 +41,31 @@ class BookList:
             self.full_series = sorted(list(
                 {item["series"] for item in sorted(self.full_info, key=lambda x: x["series"]) if item["series"]}
             ))
-            self.authors = self.full_authors
-            self.series = self.full_series
+            self.author = None
+            self.series = None
         self.data = []
         for item in sorted(self.full_info, key=lambda x: x[SORT_KEY], reverse=SORT_REVERSE):
-            author_match = not self.authors or item["author"] in self.authors
-            series_match = not self.series or not item["series"] or item["series"] in self.series
-            print(f"{item['series']} list {self.series} {series_match}")
+            author_match = not self.author or item["author"] == self.author
+            series_match = not self.series or item["series"] == self.series
             if author_match and series_match:
                 self.data.append(
                     [item["title"], item["author"], item["series"], item["date"]]
                 )
+
+    def get_filtered_list(self, opposite_key, key, filter, default):
+        if not filter:
+            return default
+        return list({
+            item[opposite_key]
+            for item in self.full_info
+            if item[key] == filter
+        })
+
+    def get_filtered_authors(self):
+        return self.get_filtered_list("author", "series", self.series, self.full_authors)
+
+    def get_filtered_series(self):
+        return self.get_filtered_list("series", "author", self.author, self.full_series)
 
     def read_data(self):
         data = None
@@ -99,7 +113,6 @@ class BookList:
                     "series": new_book.series,
                 }
             )
-            print("writing data")
             self.write_data()
             self.update_data(True)
 
@@ -107,7 +120,6 @@ class BookList:
         for item in self.full_info:
             test_book = Book(item["title"], item["author"], item["series"], item["date"])
             if test_book == old_book:
-                print("Updating: ", old_book, " to ", new_book)
                 item["title"] = new_book.title
                 item["author"] = new_book.author
                 item["date"] = new_book.date
@@ -119,29 +131,66 @@ class BookList:
         for index, item in enumerate(self.full_info):
             test_book = Book(item["title"], item["author"], item["series"], item["date"])
             if test_book == book:
-                print("Deleting: ", book, type(self.full_info))
                 del self.full_info[index]
                 self.write_data()
                 self.update_data(True)
 
     def author_filter(self, author):
-        self.authors = [author]
-        self.series = [item["series"] for item in self.full_info if item["author"] in self.authors]
+        self.author = author
         self.update_data(False)
 
     def series_filter(self, series):
-        self.series = [series]
-        self.authors = [item["author"] for item in self.full_info if item["series"] and item["series"] in self.series]
+        self.series = series
         self.update_data(False)
 
     def clear_filters(self):
-        self.authors = self.full_authors
-        self.series = self.full_series
+        self.author = None
+        self.series = None
         self.update_data(True)
 
 def delete_dialog(book):
     result = sg.popup_yes_no(f"Delete {book.title} by {book.author}?")
     return result == "Yes"
+
+def filter_dialog(title, items):
+    if len(items) == 0:
+        return ""
+    orig_items = items
+    layout = [
+        [sg.Text("Filter On:"), sg.Input(key="-FILTER-")],
+        [
+            sg.Listbox(
+                values=items,
+                default_values=items[0:1],
+                select_mode=sg.LISTBOX_SELECT_MODE_SINGLE,
+                key="-LIST-",
+                enable_events=True,
+                bind_return_key=True,
+                size=(50, 20),
+            )
+        ],
+        [sg.OK(key="Ok"), sg.Cancel(key="Cancel")],
+    ]
+    window = sg.Window(title, layout=layout, return_keyboard_events=True)
+    listbox = window["-LIST-"]
+    filter = window["-FILTER-"]
+    while True:
+        event, values = window.read()
+        # print(event, event.isalpha())
+        if event in [sg.WIN_CLOSED, "Cancel", ESC]:
+            window.close()
+            return ""
+        elif event in ["Ok", RETURN]:
+            window.close()
+            return values["-FILTER-"]
+        elif event == "-LIST-":
+            item = listbox.get()[0]
+            filter.update(item)
+        else:
+            start = filter.get()
+            items = [item for item in orig_items if item.startswith(start)]
+            listbox.update(items)
+
 
 def book_dialog(dialog_title, authors, series, book=None):
     authors_combo = sg.Combo(authors, key="-AUTH-", expand_x=True)
@@ -149,7 +198,7 @@ def book_dialog(dialog_title, authors, series, book=None):
     layout = [
         [sg.Text("Title:"), sg.Input(key="-TITLE-")],
         [
-            sg.CalendarButton("Date Finished", format="%m/%d/%Y", key="-CAL-", enable_events=True),
+            sg.CalendarButton("Date Finished", format="%Y/%m/%d", key="-CAL-", enable_events=True),
             sg.Text("Not Set", key="-DATE-"),
         ],
         [sg.Text("Author:"), authors_combo],
@@ -180,22 +229,23 @@ def book_dialog(dialog_title, authors, series, book=None):
 
         if event in ["Ok", RETURN]:
             title = values["-TITLE-"]
-            cal = window["-DATE-"].get()
-            if cal == "Not Set":
-                cal = ""
             author = values["-AUTH-"]
-            series = values["-SER-"]
-            window.close()
             if title and author:  # must supply title and author
+                cal = window["-DATE-"].get()
+                if cal == "Not Set":
+                    cal = ""
+                series = values["-SER-"]
+                window.close()
                 return Book(title, author, series, cal)
+            window.close()
             return None
 
 
 def update_ui(window, books):
-    window["-AUTHORS-"].update(books.authors)
-    window["-SERIES-"].update(books.series)
-    window["-BOOKTABLE-"].update(books.data)
+    window["-AUTHOR-FILTER-"].update(books.author or "None")
+    window["-SERIES-FILTER-"].update(books.series or "None")
     table = window["-BOOKTABLE-"]
+    table.update(books.data)
     if len(table.Values):
         table.update(select_rows=[0])
 
@@ -204,10 +254,8 @@ books = BookList()
 
 layout = [
     [
-        sg.Text("Filter by author:"),
-        sg.Listbox(values=books.authors, size=(50, 10), key="-AUTHORS-", enable_events=True),
-        sg.Text("Filter by series:"),
-        sg.Listbox(values=books.series, size=(50, 10), key="-SERIES-", enable_events=True),
+        sg.Button("Author Filter:"), sg.Text("None", key="-AUTHOR-FILTER-", size=20),
+        sg.Button("Series Filter:"), sg.Text("None", key="-SERIES-FILTER-", size=20),
     ],
     [
         sg.Table(
@@ -239,12 +287,15 @@ table.bind("<Button-1>", "Click")
 
 while True:
     event, values = window.read()
+    # For some reason, keyboard events are coming in the form of "a:38" or "b:56"
+    # Split out the keyboard code
+    if event and ":" in event:
+        event = event.split(":")[0]
     # print(event, values)
     if event in [sg.WIN_CLOSED, "Exit", ESC]:
         break
     elif event == 'Clear Filters':
         books.clear_filters()
-        update_ui(window, books)
     elif event == '-BOOKTABLE-Click':
         e = table.user_bind_event
         region = table.Widget.identify('region', e.x, e.y)
@@ -253,32 +304,36 @@ while True:
             column = int(table.Widget.identify_column(e.x)[1:]) - 1
             SORT_KEY = sort_indices[column]
             SORT_REVERSE = not SORT_REVERSE
-            update_ui(window, books)
     elif event == '-AUTHORS-':
         books.author_filter(values["-AUTHORS-"][0])
-        update_ui(window, books)
     elif event == '-SERIES-':
         books.series_filter(values["-SERIES-"][0])
-        update_ui(window, books)
     elif event in ["Add", "a", "A"]:
         new_book = book_dialog("Add Book", books.full_authors, books.full_series)
         if new_book is not None:
             books.add_book(new_book)
-            update_ui(window, books)
     elif event in ["Edit", "e", "E"]:
         if table and table.SelectedRows:
-            if table.SelectedRows:
-                book = Book(*table.Values[table.SelectedRows[0]])
-                new_book = book_dialog("Edit Book", books.full_authors, books.full_series, book)
-                if new_book is not None:
-                    books.edit_book(book, new_book)
-                    update_ui(window, books)
+            book = Book(*table.Values[table.SelectedRows[0]])
+            new_book = book_dialog("Edit Book", books.full_authors, books.full_series, book)
+            if new_book is not None:
+                books.edit_book(book, new_book)
     elif event in ["Delete", "d", "D"]:
         if table and table.SelectedRows:
-            if table.SelectedRows:
-                book = Book(*table.Values[table.SelectedRows[0]])
-                if delete_dialog(book):
-                    books.delete_book(book)
-                    update_ui(window, books)
+            book = Book(*table.Values[table.SelectedRows[0]])
+            if delete_dialog(book):
+                books.delete_book(book)
+    elif event == "Author Filter":
+        authors = books.get_filtered_authors()
+        val = filter_dialog("authors", authors)
+        if val and len(val) != 0:
+            books.author_filter(val)
+    elif event == "Series Filter":
+        series = books.get_filtered_series()
+        val = filter_dialog("series", series)
+        if val and len(val) != 0:
+            books.series_filter(val)
+    if event != "-BOOKTABLE-":
+        update_ui(window, books)
 
 window.close()
